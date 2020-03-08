7b6c978b05a898ece2b3fbb5fd1c048774574cdd
Dep zai <hieudankaz@gmail.com>
2020-03-08 16:57:15 +0700

    Header html css next JS

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var ListActive=document.getElementsByClassName('active-click');
+var Iconactive=document.getElementById('id-nav');
+var menu=document.getElementById('id-nav-menu');
 for(let i of ListActive)
 	i.addEventListener("click",function(){
 		var child=this.childNodes[2];
@@ -8,3 +10,20 @@ for(let i of ListActive)
 		else
 			child.style.display="none";
 	});
+Iconactive.addEventListener("click",function(){
+
+	if(menu.style.display=="none")
+	{
+		this.childNodes[1].style.transform="rotate(30deg)";
+		this.childNodes[3].style.opacity="0";
+		this.childNodes[5].style.transform="rotate(-30deg)";
+		menu.style.display="block";
+	}
+	else
+	{
+		this.childNodes[1].style.transform="rotate(0deg)";
+		this.childNodes[3].style.opacity="1";
+		this.childNodes[5].style.transform="rotate(0deg)";
+		menu.style.display="none";
+	}
+});
